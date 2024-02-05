ecc0afd7e3559d314deaed35f358c790be111f64
obetta1 <obettabenjamin44@gmail.com>
2024-02-05 15:31:02 +0100

    fix error message

--- lib/data/apiClient/api_client.dart
+++ lib/data/apiClient/api_client.dart
@@ -52,7 +52,7 @@ class ApiClient {
   Future<PopularCarMakes> getPopularCarMakes() async {
     ProgressDialogUtils.showProgressDialog();
     try {
-      await isNetworkConnected();
+      //await isNetworkConnected();
       var response = await _dio.get(
         '$baseUrl${UrlConfig.popularMakes}',
         options: Options(
@@ -84,7 +84,6 @@ class ApiClient {
   Future<AllCarModel> getAllCars() async {
     ProgressDialogUtils.showProgressDialog();
     try {
-      await isNetworkConnected();
       var response = await _dio.get(
         '$baseUrl${UrlConfig.allCars}',
         options: Options(
